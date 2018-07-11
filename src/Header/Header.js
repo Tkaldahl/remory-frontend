8f56e620afe4c333391249d4101aaf11f728953c
Daniel Lousa <dlousa92@gmail.com>
2018-07-11 12:08:18 -0400

    give header semantic classnames for styling and choose colors

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,8 +5,8 @@ class Header extends React.Component {
   render () {
     return (
       <div className='header'>
-        <h1>Remory</h1>
-        <img src='#' alt='Remory Logo' /> {/* Add in this image source once we've picked our logo */}
+        <h1 className='sitetitle'>Remory</h1>
+        <img className='logo' src='#' alt='Remory Logo' /> {/* Add in this image source once we've picked our logo */}
       </div>
     )
   }
