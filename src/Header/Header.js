5eb4585a9f330d31c6323e29f517c83ca5fa8b9d
Daniel Lousa <dlousa92@gmail.com>
2018-07-11 11:44:41 -0400

    import css file for header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Header.css'
 
 class Header extends React.Component {
   render () {
     return (
-      <div className='HeaderContainer'>
+      <div className='header'>
         <h1>Remory</h1>
         <img src='#' alt='Remory Logo' /> {/* Add in this image source once we've picked our logo */}
       </div>
