60fa797f8c4f36e442242d0d17161574dd442f24
Taylor Kaldahl <tkaldahl90@gmail.com>
2018-07-11 15:23:19 -0400

    Add memory title per Daniel's review

--- src/MemorySquare/MemorySquare.js
+++ src/MemorySquare/MemorySquare.js
@@ -5,6 +5,7 @@ class MemorySquare extends React.Component {
   render () {
     return (
       <div className='MemorySquareContainer'>
+        <h3>{this.props.memory.title}</h3>
         <Link to={`/memory/${this.props.memory._id}`}><img src={this.props.memory.imageURL} /></Link>
       </div>
     )
