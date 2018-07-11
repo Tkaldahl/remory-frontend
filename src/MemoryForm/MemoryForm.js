eb123b19632c08ab55367f4bb005718186b175f7
Daniel Lousa <dlousa92@gmail.com>
2018-07-11 15:42:47 -0400

    create functions to listen to input fields

--- src/MemoryForm/MemoryForm.js
+++ src/MemoryForm/MemoryForm.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import axios from 'axios'
 
 class MemoryForm extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      titleString: '',
+      imageURL: '',
+      postString: ''
+    }
+  }
   render () {
     return (
       <div>
         <h2>New Memory</h2>
-        <form>
-          <input type='text' placeholder='Title?' />
-          <input type='text' placeholder='Image Url?' />
-          <textarea rows='8' cols='50'>Memory Details...</textarea>
+        <form onSubmit={this.onClick}>
+          <input type='text' placeholder='Title?' onChange={this.setTitle} />
+          <input type='text' placeholder='Image Url?' onChange={this.setImage} />
+          <textarea rows='8' cols='50' onChange={this.setDetails}>Memory Details...</textarea>
           <input type='button' value='Submit Memory' />
         </form>
       </div>
