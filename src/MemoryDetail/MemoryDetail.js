b5ea3b35cc8e09a03c75ddd80ae28b72d15a04fd
Taylor Kaldahl <tkaldahl90@gmail.com>
2018-07-11 15:40:20 -0400

    Added MemoryDetail Structure

--- src/MemoryDetail/MemoryDetail.js
+++ src/MemoryDetail/MemoryDetail.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import CommentForm from '../CommentForm/CommentForm'
 
+// Notes: This component will receive props from App.js. MemorySquare should have an event listener which 'hears' that it's been clicked, sets the state of App.js to select a specific memory, and then App.js passes that memoryId to memoryDetail as a prop.
+
 class MemoryDetail extends React.Component {
   render () {
     return (
-      <div>HelloWorld</div>
+      <div className='MemoryDetailContainer'>
+        <img src={this.props.memory.imageURL} />
+        <h2>{this.props.memory.title}</h2>
+        <p>{this.props.memory.postString} </p>
+        <CommentForm />
+      </div>
     )
   }
 }
