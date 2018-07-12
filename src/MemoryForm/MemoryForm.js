452d3dc25509977624ce8a12c96ba59bac284f4c
Isaiah Berg <isaiah.j.berg@gmail.com>
2018-07-12 12:39:36 -0400

    CRUD with add memory and add user at localhost:4000/memory and /user tests and validates

--- src/MemoryForm/MemoryForm.js
+++ src/MemoryForm/MemoryForm.js
@@ -17,17 +17,19 @@ class MemoryForm extends React.Component {
     this.setImage = this.setImage.bind(this)
     this.setDetails = this.setDetails.bind(this)
     this.setEmail = this.setEmail.bind(this)
+    this.onClick = this.onClick.bind(this)
+    this.createMemory = this.createMemory.bind(this)
   }
   render () {
     return (
       <div className='formcontainer'>
         <h2 className='subheading'>New Memory</h2>
         <form className='form'>
-          <input classname='inputfield' type='text' placeholder='Title?' />
-          <input classname='inputfield' type='text' placeholder='Image Url?' />
-          <textarea classname='inputfield' rows='8' cols='50'>Memory Details...</textarea>
-          <input classname='inputfield' type='text' placeholder='Author Email?' />
-          <input type='button' value='Create Memory' />
+          <input className='inputfield' type='text' placeholder='Title?' onChange={this.setTitle} />
+          <input className='inputfield' type='text' placeholder='Image Url?' onChange={this.setImage} />
+          <textarea className='inputfield' rows='8' cols='50' placeholder='Memory Details...' onChange={this.setDetails} />
+          <input className='inputfield' type='text' placeholder='Author Email?' onChange={this.setEmail} />
+          <input type='button' value='Create Memory' onClick={this.onClick} />
         </form>
       </div>
     )
@@ -36,36 +38,44 @@ class MemoryForm extends React.Component {
     this.setState({
       titleString: e.target.value
     })
+    console.log(this.state.titleString)
   }
   setEmail (e) {
     this.setState({
       authorEmail: e.target.value
     })
+    console.log(this.state.authorEmail)
   }
   setImage (e) {
     this.setState({
       imageURL: e.target.value
     })
+    console.log(this.state.imageURL)
   }
   setDetails (e) {
     this.setState({
       postString: e.target.value
     })
+    console.log(this.state.postString)
   }
   onClick (e) {
     this.createMemory(e)
   }
   createMemory (e) {
     e.preventDefault()
-
-    axios.post('http://localhost/4000/memory', {
+    console.log('AXIOS POST CREATEMEMORY()')
+    axios.post('http://localhost:4000/memory', {
       titleString: this.state.titleString,
       imageURL: this.state.imageURL,
       postString: this.state.postString,
       authorEmail: this.state.authorEmail
     })
       .then((res) => {
+        console.log(res.data)
         this.props.history.push('/')
+      })
+      .catch((err) => {
+        console.log(err.response)
       })
   }
 }
